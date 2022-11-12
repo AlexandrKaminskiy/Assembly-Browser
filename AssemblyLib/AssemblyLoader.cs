e6e6778d0cb71e7d25183e510c76b901fd872d90
AlexandrKaminskiy <sasha.pinsk2003@gmail.com>
2022-11-12 03:24:00 +0300

    Add project files.

---
+++ AssemblyLib/AssemblyLoader.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyLib
+{
+    public class AssemblyLoader
+    {
+        private readonly string path = "../../../../Assemblies/";
+        public string s;
+        public string q { get; set; }
+        public void LoadAssembly()
+        {
+            Assembly assembly = Assembly.LoadFrom(path + "AssemblyLib.dll");
+            string name = assembly.GetName().Name;
+            var types = assembly.GetTypes();
+            Console.WriteLine(assembly.GetTypes()[0].Name);
+            var meths = assembly.GetTypes()[0].GetMethods();
+            var fields = assembly.GetTypes()[0].GetFields();
+            var props = assembly.GetTypes()[0].GetProperties();
+
+            foreach (var prop in props)
+            {
+                string s = propInfo(prop);
+                Console.WriteLine(s);
+            }
+
+            foreach (var field in fields)
+            {
+                string s = fieldInfo(field);
+                Console.WriteLine(s);
+            }
+
+            foreach (var meth in meths)
+            {
+                string s = methodSignature(meth);
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine();
+        }
+
+        private string propInfo(PropertyInfo prop)
+        {
+            return prop.PropertyType + " " + prop.Name;
+        }
+
+        private string fieldInfo(FieldInfo fieldInfo)
+        {
+            return fieldInfo.FieldType + " " + fieldInfo.Name;
+        }
+        public string methodSignature(MethodInfo methodInfo)
+        {
+            ParameterInfo[] parameters = methodInfo.GetParameters();
+            string result = methodInfo.Name + "(";
+
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                result += parameters[i].GetType().Name + " " + parameters[i].Name;
+                if (i < parameters.Length - 1)
+                {
+                    result += ", ";
+                }
+            }
+
+            result += ")";
+
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using AssemblyLib;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AssemblyLoader asseblyLoader = new AssemblyLoader();
+            asseblyLoader.LoadAssembly();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
